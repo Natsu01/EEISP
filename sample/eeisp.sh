e06673316ae0d62bbb05bcde60fcbcb184ef6d8a
nakatolab <nakatolab.members@gmail.com>
2021-09-25 23:58:33 +0900

    version 0.3.0

--- sample/eeisp.sh
+++ sample/eeisp.sh
@@ -2,7 +2,7 @@
 
 threCDI=0.5
 threEEI=0.5
-../eeisp.py data.txt Sample --threCDI $threCDI --threEEI $threEEI
+eeisp data.txt Sample --threCDI $threCDI --threEEI $threEEI
 
-../add_genename_from_geneid.py Sample_CDI_score_data_thre$threCDI.txt Sample_CDI_score_data_thre$threCDI.addgenename.txt geneidlist.txt
-../add_genename_from_geneid.py Sample_EEI_score_data_thre$threEEI.txt Sample_EEI_score_data_thre$threEEI.addgenename.txt geneidlist.txt
+eeisp_add_genename_from_geneid Sample_CDI_score_data_thre$threCDI.txt Sample_CDI_score_data_thre$threCDI.addgenename.txt geneidlist.txt
+eeisp_add_genename_from_geneid Sample_EEI_score_data_thre$threEEI.txt Sample_EEI_score_data_thre$threEEI.addgenename.txt geneidlist.txt
