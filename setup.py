d1a9d6b14b5d673a3c4d6365d057a82f2e39bb0d
nakatolab <nakatolab.members@gmail.com>
2021-09-25 23:55:44 +0900

    version 0.3.0

--- setup.py
+++ setup.py
@@ -5,9 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="eeisp",
-    version="0.3.0",
+    version="0.3.5",
     license="GPL3.0",
-    install_requires=['numpy','pandas','scipy','math','multiprocessing','time'],
+    install_requires=[
+        "numpy>=1.14.2",
+        "pandas>=0.22.0",
+        "scipy>=1.3"
+    ],
     author="Natsu Nakajima, Ryuichiro Nakato",
     author_email="rnakato@iqb.u-tokyo.ac.jp",
     description="Identify gene pairs that are codependent and mutually exclusive from single-cell RNA-seq data.",
@@ -15,10 +19,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/nakatolab/EEISP",
     keywords="eeisp scRNA-seq",
+    scripts=['eeisp/eeisp', 'eeisp/eeisp_add_genename_from_geneid'],
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPL-3.0 License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
